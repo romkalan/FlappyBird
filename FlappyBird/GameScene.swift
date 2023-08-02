56825a9a5497998bdb6ba611e5f1de39033b1365
romkalan <romkalan@mail.ru>
2023-08-02 15:58:16 +0300

    add player on gameScene and other objects

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -8,81 +8,59 @@
 import SpriteKit
 import GameplayKit
 
+enum GameStatus {
+    case idle // инициализация
+    case running // запуск
+    case over // окончание
+}
+
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var gameStatus: GameStatus = .idle
+    
+    var base1: SKSpriteNode!
+    var base2: SKSpriteNode!
+    var player: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor(red: 80/255, green: 192/255, blue: 203/255, alpha: 1.0)
+        addBase()
+        addPlayer()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
+    }
+    
+    private func addBase() {
+        base1 = SKSpriteNode(imageNamed: "base")
+        base1.anchorPoint = CGPoint(x: 0, y: 0)
+        base1.position = CGPoint(x: 0, y: 0)
+        addChild(base1)
+        
+        base2 = SKSpriteNode(imageNamed: "base")
+        base2.anchorPoint = CGPoint(x: 0, y: 0)
+        base2.position = CGPoint(x: base1.size.width, y: 0)
+        addChild(base2)
+    }
+    
+    private func addPlayer() {
+        player = SKSpriteNode(imageNamed: "redbird-midflap")
+        player.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        addChild(player)
+    }
+    
+    // GameStatus methods
+    private func shuffle() {
+        gameStatus = .idle
+    }
+    
+    private func startGame() {
+        gameStatus = .running
+    }
+    
+    private func gameOver() {
+        gameStatus = .over
     }
 }
